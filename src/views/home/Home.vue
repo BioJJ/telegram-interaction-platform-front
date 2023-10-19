48ef5ed5d549fd37057e8f7d68af569933361227
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-10-19 18:52:13 -0400

    refactor: ♻️ add view and models

---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-container id="home" class="pa-6" fluid>
+		<v-row class="mb-3">
+			<v-col cols="6">
+				<v-breadcrumbs divider="|" class="pa-0 breadcrumbs-title">
+					{{ $route.meta.title }}
+				</v-breadcrumbs>
+			</v-col>
+			<v-col cols="6"> HOME</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: 'Home',
+	components: {}
+})
+</script>
+
+<style></style>
---
+++ src/views/user/User.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>User</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: 'User'
+})
+</script>
+
+<style></style>
